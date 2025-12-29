0a5e21cac93ab83ffa2860b951720dc0d69bf77a
Rbel12b <rbel12b@gmail.com>
2025-12-29 13:42:32 +0100

    Implement pathToFileName and log_printf functions for logging functionality

--- lib/Lpf2/log/log.cpp
+++ lib/Lpf2/log/log.cpp
@@ -4,12 +4,24 @@
 
 const char *pathToFileName(const char *path)
 {
-    return nullptr;
+    int i = 0;
+    for (int i = strlen(path) - 1; i >= 0; i--)
+    {
+        if (path[i] == '/' || path[i] == '\\')
+        {
+            break;
+        }
+    }
+    return path + i + 1;
 }
 
 int log_printf(const char *fmt, ...)
 {
-    return 0;
+    va_list args;
+    va_start(args, fmt);
+    int result = vfprintf(stdout, fmt, args);
+    va_end(args);
+    return result;
 }
 
 #endif // LPF2_LOG_IMPL
