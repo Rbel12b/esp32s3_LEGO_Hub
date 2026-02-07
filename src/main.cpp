018ecb4ae6b3a66a61c6d6cf7111dd3373c52fca
Rbel12b <rbel12b@gmail.com>
2026-02-07 09:18:54 +0100

    update lib

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #include "Lpf2Virtual/Lpf2PortVirtual.h"
 #include "Lpf2Virtual/Lpf2VirtualDevice.h"
 #include "Lpf2DeviceDescLib.h"
+#include "Lpf2Devices/ColorSensor.h"
 
 extern "C" int serial_vprintf(const char *fmt, va_list args)
 {
@@ -40,7 +41,8 @@ void setup()
     Serial.begin(981200);
     lpf2_log_printf("booted.");
 
-    Lpf2DeviceRegistry::registerDefaultFactories();
+    Lpf2DeviceRegistry::registerDefault();
+    Lpf2DeviceDescRegistry::registerDefault();
 
     esp_log_set_vprintf(serial_vprintf);
 
@@ -69,6 +71,15 @@ void loop()
     vTaskDelay(1);
 
     updatePorts();
+    if (portB.deviceConnected())
+    {
+        auto s = portB.getInfoStr();
+        for (size_t i = 0; i < s.size(); i += 128)
+        {
+            Serial.write(s.data() + i, std::min<size_t>(128, s.size() - i));
+            delay(1); // let other tasks breathe
+        }
+    }
 #if EMULATE_HUB == 1
     vHub.update();
 
@@ -89,15 +100,6 @@ void loop()
     if (portA.getDeviceType() == Lpf2DeviceType::TECHNIC_LARGE_LINEAR_MOTOR)
     {
         ESP.restart();
-    }
-    else if (portA.deviceConnected())
-    {
-        auto s = portA.getInfoStr();
-        for (size_t i = 0; i < s.size(); i += 128)
-        {
-            Serial.write(s.data() + i, std::min<size_t>(128, s.size() - i));
-            delay(1); // let other tasks breathe
-        }
     }
 #else
     if (!realHub.isConnected() && !realHub.isConnecting())
@@ -133,6 +135,63 @@ void loop()
                 delay(1); // let other tasks breathe
             }
             printedInfos = true;
+        }
+
+        auto &r_portA = *realHub.getPort(Lpf2PortNum(Lpf2ControlPlusHubPort::A));
+        if (r_portA.deviceConnected())
+        {
+            static bool portASetupDone = false;
+            if (!portASetupDone)
+            {
+                r_portA.setMode(0);
+                portASetupDone = true;
+            }
+
+            if (r_portA.getDeviceType() == Lpf2DeviceType::TECHNIC_COLOR_SENSOR)
+            {
+                auto device = new TechnicColorSensor(r_portA);
+    
+                switch (device->getColorIdx())
+                {
+                case Lpf2ColorIDX::BLACK:
+                    BuitlInRGB_setColor(0, 0, 0);
+                    break;
+                case Lpf2ColorIDX::BLUE:
+                    BuitlInRGB_setColor(0, 0, 50);
+                    break;
+                case Lpf2ColorIDX::GREEN:
+                    BuitlInRGB_setColor(0, 50, 0);
+                    break;
+                case Lpf2ColorIDX::RED:
+                    BuitlInRGB_setColor(50, 0, 0);
+                    break;
+                case Lpf2ColorIDX::WHITE:
+                    BuitlInRGB_setColor(50, 50, 50);
+                    break;
+                case Lpf2ColorIDX::YELLOW:
+                    BuitlInRGB_setColor(50, 50, 0);
+                    break;
+                case Lpf2ColorIDX::ORANGE:
+                    BuitlInRGB_setColor(50, 20, 0);
+                    break;
+                case Lpf2ColorIDX::PURPLE:
+                    BuitlInRGB_setColor(30, 0, 30);
+                    break;
+                case Lpf2ColorIDX::PINK:
+                    BuitlInRGB_setColor(50, 0, 20);
+                    break;
+                case Lpf2ColorIDX::LIGHTBLUE:
+                    BuitlInRGB_setColor(0, 20, 50);
+                    break;
+                case Lpf2ColorIDX::CYAN:
+                    BuitlInRGB_setColor(0, 50, 50);
+                    break;
+    
+                default:
+                    BuitlInRGB_setColor(0, 0, 0);
+                    break;
+                }
+            }
         }
     }
 #endif
